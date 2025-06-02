586650135f861285c1aaf9dbb9ddc9ff331fa3ea
notsoexpert <notsoexpert@gmail.com>
2025-06-02 15:22:50 -0500

    Added cleanInputs function to main.go and added tests in repl_test.go. Began implementing CLI in main function.

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-imports "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			text := scanner.Text()
+			cleanedInput := cleanInput(text)
+			if len(cleanedInput) > 0 {
+				fmt.Printf("Your command was: %s\n", cleanedInput[0])
+			}
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	text = strings.ToLower(text)
+	text = strings.TrimSpace(text)
+	splitInput := strings.Split(text, " ")
+	cleanedInput := []string{}
+	for _, str := range splitInput {
+		if str != "" {
+			cleanedInput = append(cleanedInput, str)
+		}
+	}
+	return cleanedInput
 }
---
+++ repl_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    " ApPlE ChErRy PEACH banana                ",
+			expected: []string{"apple", "cherry", "peach", "banana"},
+		},
+		{
+			input:    "                                    TEST l",
+			expected: []string{"test", "l"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Output slice length != expected slice length")
+			return
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("Output word doesn't match expected word")
+				return
+			}
+		}
+	}
+}
